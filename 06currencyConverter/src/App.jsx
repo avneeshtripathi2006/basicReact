fb7d045c402cbfa7914bc09f60d8fbaca4e33a85
Avneesh Tripathi <avneeshtripathi.2006@gmail.com>
2025-09-02 09:40:56 +0530

    Incomplete currency converter added

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -6,9 +6,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="min-h-[50vmin] min-w-[75vmin] bg-slate-300 border-[5px] border-slate-500 rounded-[20px] opacity-70 flex flex-col justify-center items-center gap-[2vmin] shadow-[40px_40px_0px_#bebebe,-40px_-40px_60px_#ffffff]">
-      <Box/>
-      <Box/>
+    <div className="min-h-[50vmin] min-w-[75vmin] bg-slate-300 border-[5px] border-slate-500 rounded-[20px] opacity-70 flex flex-col justify-center items-center gap-[2vmin] shadow-[40px_40px_60px_#bebebe,-40px_-40px_60px_#ffffff]">
+      <Box id="From"/>
+      <Box id="To"/>
+      <button className="min-w-[65vmin] min-h-[7vmin] bg-blue-700 text-white font-bold">Convert</button>
     </div>
   )
 }
--- 06currencyConverter/src/components/Box.jsx
+++ 06currencyConverter/src/components/Box.jsx
@@ -1,7 +1,15 @@
-function Box(props){
-    return(
-        <div className="min-h-[15vmin] min-w-[65vmin] bg-white border-2 rounded-[7px] shadow-lg">
-
+function Box(props) {
+    return (
+        <div className="min-h-[15vmin] min-w-[65vmin] bg-white border-2 rounded-[7px] shadow-lg flex flex-col justify-evenly">
+            <div className="flex flex-row justify-around items-center">
+                <label htmlFor="propid">{props.id}</label>
+                <label htmlFor="code">Currency Type</label>
+                
+            </div>
+            <div className="flex flex-row justify-around items-center">
+                <input id="propid" type="number" defaultValue={0} className="w-[10vmin]"></input>
+                <select id="code"></select>
+            </div>
         </div>
     );
 }
